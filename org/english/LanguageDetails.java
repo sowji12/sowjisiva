18f21c1936c68bb07a0c7969ac5a99afdabed824
sowji <sowjanyag873@gmail.com>
2019-08-19 10:14:59 +0530

    message

---
+++ org/english/LanguageDetails.java
@@ -0,0 +1,12 @@
+package org.english;
+
+public class LanguageDetails {
+	public void English() {
+		System.out.println("english");
+	}
+	public static void main(String[] args) {
+		LanguageDetails e = new LanguageDetails();
+		e.English();
+	}
+
+}
